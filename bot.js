fd7b8a48a1e43f2fe2e2183353efdd044d1d3315
MedoGamez <68783270+MedoGamez@users.noreply.github.com>
2020-07-25 17:45:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['Your ID']
+const myid = [515584004746117140]
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
